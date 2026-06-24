a91f45870317b81fbf9dfda071e987a4ee274b3f
Your Name <you@example.com>
2026-06-24 16:43:15 -0500

    initialized ascii image project

---
+++ asciiator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+std::string stringOfASCII = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."; //size: 69 
+
+/*
+ * Goal: input an image w/ desired scaling. Outputs an AxB array (with each line ending in /n) of ASCII characters that 
+ * correspond to the shading of the image
+ *
+ * 
+ *
+ *
+ */
+
+int createBufferImage() {
+// Create a buffer for our image.
+
+};
+
+int getASCIIValue() {
+// Get the ASCII Value of a grouping of pixels
+
+}
+
+int scaleImage() {
+// Scale buffer image
+
+}
+
+int main() {
+  ofstream file;
+
+  file.open("example.png")
+  
+  return 0;
+};
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    cmake
+                    codespell
+                    conan
+                    cppcheck
+                    doxygen
+                    gtest
+                    lcov
+					opencv
+				    vcpkg
+                    vcpkg-tool
+                    self.formatter.${system}
+                  ]
+                  ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ gdb ];
+              };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
